8a4def281d9d298aa4a7461b51351e8fe6984e65
Moisés de Jesús Cortés Castellanos <ing.moisesdejesuscortesc@notsoweb.com>
2023-09-12 16:58:46 -0600

    ADD: Errores voluntarios para atributos especificos

--- src/app/Support/Vue/NotifySupport.php
+++ src/app/Support/Vue/NotifySupport.php
@@ -43,6 +43,32 @@ class NotifySupport
     }
 
     /**
+     * Notifica un error especifico
+     * 
+     * Se genera un error voluntariamente para que inertia lo pueda interpretar mandando
+     * un notificación tipo toast. 
+     *
+     * El error se recibe dentro del método javascript perteneciente a router del paquete @inertiajs/vue3:
+     * onError: (e) => Notify.fromBack(e),
+     * 
+     * Importante: No usar junto a los formularios.
+     * 
+     * Puede ser usado también para blade.
+     * 
+     * @param string $model Atributo al que se le imputara el error
+     * @param string $message Mensaje de error
+     * @param string $varName Variable que se recibirá en el front para el error
+     */
+    public static function errorIn($model, $message) : void
+    {
+        throw new ValidationException(Validator::make([], [
+            $model => 'required',
+        ], [
+            $model => $message,
+        ]));
+    }
+
+    /**
      * Notifica una advertencia
      * 
      * Se genera un error voluntariamente para que inertia lo pueda interpretar mandando
@@ -66,6 +92,34 @@ class NotifySupport
         ], [
             'errorMessage' => $message,
             'type' => 'warning'
+        ]));
+    }
+
+    /**
+     * Notifica una advertencia especifica
+     * 
+     * Se genera un error voluntariamente para que inertia lo pueda interpretar mandando
+     * un notificación tipo toast. 
+     *
+     * La advertencia se recibe dentro del método javascript perteneciente a router del paquete @inertiajs/vue3:
+     * onError: (e) => Notify.fromBack(e),
+     * 
+     * La variable puede variar si se desea.
+     * 
+     * Importante: No usar junto a los formularios.
+     * 
+     * Puede ser usado también para blade.
+     * 
+     * @param string $model Atributo al que se le imputara el error
+     * @param string $message Mensaje de advertencia
+     * @param string $varName Variable que se recibirá en el front para el error
+     */
+    public static function warningIn($model, $message) : void
+    {
+        throw new ValidationException(Validator::make([], [
+            $model => 'required',
+        ], [
+            $model => $message,
         ]));
     }
 }
